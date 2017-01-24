a774724869324952c621ed5aecb53712763a8278
Christoph Friedrich <christoph@friedrich-whv.de>
2017-01-24 14:36:20 +0100

    code cleanup, readme extension

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # NRWAIR
 
-read: "unaware" because you're unaware of what particles are in the air! ;-)
+read: "unaware" -- because you're unaware of what particles are in the air! ;-)
 
-Project in 3rd semester Spatial Information Infrastructures (SII) course at ifgi Münster
+This webapp combines Esri Basemaps (via WMTS) with air quality data from the Umweltbundesamt (via SOS, self-hosted). These are the files to put into the /var/lib/tomcat8/webapps/ROOT/ folder of the 52° North SOS server implementation.
+
+Written as a project for the Spatial Information Infrastructures (SII) course during by B.Sc. Geoinformatics studies at Institute for Geoinformatics (ifgi) of Münster University (WWU).
--- app.js
+++ app.js
@@ -5,14 +5,15 @@ var baseurlapi = '/52n-sos-webapp/service';
 
 // global variables
 var currX, currY, currZoom = 0;
-var maxX, maxY, maxZoom = 0;
+var maxX,  maxY,  maxZoom  = 0;
 var ajax;
 
-function log() {
-  document.getElementById('log').innerHTML = 'X=' + currX + ' <br> ' +
-                                             'Y=' + currY + ' <br> ' +
-                                             'Zoom=' + currZoom + ' <br> ' +
-                                             'Zeit=' + new Date(parseInt(document.getElementById('time').value+'000')).toLocaleDateString();
+function displayCurrentMapParameters() {
+  document.getElementById('mapparameters').innerHTML =
+    'X=' + currX + ' <br> ' +
+    'Y=' + currY + ' <br> ' +
+    'Zoom=' + currZoom + ' <br> ' +
+    'Zeit=' + new Date(parseInt(document.getElementById('time').value+'000')).toLocaleDateString();
 }
 
 function showTile(zoom, x, y) {
@@ -36,8 +37,7 @@ function showTile(zoom, x, y) {
   document.getElementById('lowermiddle2').src  = baseurlref + 'tilematrix=' + zoom + '&tilerow=' + (x+1) + '&tilecol=' + y;
   document.getElementById('lowerright2').src   = baseurlref + 'tilematrix=' + zoom + '&tilerow=' + (x+1) + '&tilecol=' + (y+1);
   
-  log();
-  
+  displayCurrentMapParameters();
   updateMarkers();
 }
 
@@ -46,49 +46,69 @@ function showCurrTile() {
 }
 
 function up() {
-  if(currX != 1) currX -= 1;
-  showCurrTile();
+  if(currX != 0) {
+    currX -= 1;
+    showCurrTile();
+  }
 }
 
 function down() {
-  if(currX != maxX) currX += 1;
-  showCurrTile();
+  if(currX != maxX) {
+    currX += 1;
+    showCurrTile();
+  }
 }
 
 function left() {
-  if(currY != 1) currY -= 1;
-  showCurrTile();
+  if(currY != 0) {
+    currY -= 1;
+    showCurrTile();
+  }
 }
 
 function right() {
-  if(currY != maxY) currY += 1;
-  showCurrTile();
+  if(currY != maxY) {
+    currY += 1;
+    showCurrTile();
+  }
 }
 
 function zoomIn() {
-  if(currZoom != maxZoom) currZoom += 1;
-  currX *= 2;
-  currY *= 2;
-  showCurrTile();
+  if(currZoom != maxZoom) {
+    currZoom += 1;
+    currX *= 2;
+    currY *= 2;
+    maxX *= 2 || 1;
+    maxY *= 2 || 1;
+    showCurrTile();
+  }
 }
 
 function zoomOut() {
-  if(currZoom != 0) currZoom -= 1;
-  currX = Math.floor(currX / 2);
-  currY = Math.floor(currY / 2);
-  showCurrTile();
-}
-
-function timeBackward() {
-  var slider = document.getElementById('time');
-  if(slider.value != slider.min) slider.value = parseInt(slider.value) - parseInt(slider.step);
-  slider.onchange();
+  if(currZoom != 1) {
+    currZoom -= 1;
+    currX = Math.floor(currX / 2);
+    currY = Math.floor(currY / 2);
+    maxX = Math.floor(maxX / 2);
+    maxY = Math.floor(maxY / 2);
+    showCurrTile();
+  }
 }
 
 function timeForward() {
-  var slider = document.getElementById('time');
-  if(slider.value != slider.max) slider.value = parseInt(slider.value) + parseInt(slider.step);
-  slider.onchange();
+  if(slider.value != slider.max) {
+    var slider = document.getElementById('time');
+    slider.value = parseInt(slider.value) + parseInt(slider.step);
+    slider.onchange();
+  }
+}
+
+function timeBackward() {
+  if(slider.value != slider.min) {
+    var slider = document.getElementById('time');
+    slider.value = parseInt(slider.value) - parseInt(slider.step);
+    slider.onchange();
+  }
 }
 
 // http://gis.stackexchange.com/q/153839
@@ -99,19 +119,6 @@ function latLonToPixels(lat, lon) {
   //document.getElementById('punkt').style.top = (pixelY+10-24) + 'px';
   //document.getElementById('punkt').style.left = (pixelX+10-12) + 'px';
   return [pixelX, pixelY];
-}
-
-function addMarkerAlt(lat, lon) {
-  var pixelcoords = latLonToPixels(lat, lon);
-  var marker = document.createElement("div");
-  marker.innerHTML = '<svg baseProfile="basic" xmlns="http://www.w3.org/2000/svg" width="24" height="26" viewBox="0 0 48 48"><path d="M24 0c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z"></path></svg>';
-  marker.classList.add('marker');
-  //marker.style.position = "relative";
-  marker.style.left = (pixelcoords[0]) + 'px';
-  marker.style.top  = (pixelcoords[1]) + 'px';
-  marker.dataset.lat = lat;
-  marker.dataset.lon = lon;
-  document.getElementById('markers').appendChild(marker);
 }
 
 function addMarker(lat, lon, value, caption) {
@@ -138,36 +145,13 @@ function updateMarkers() {
   )
 }
 
-function handleShortcuts(e) {
-  switch(e.key)
-  {
-    case '+': zoomIn(); break;
-    case '-': zoomOut(); break;
-    case 'w': up(); break;
-    case 'a': left(); break;
-    case 's': down(); break;
-    case 'd': right(); break;
-    case 'q': timeBackward(); break;
-    case 'e': timeForward(); break;
-  }  
-}
-
-function init() {
-  //showTile(6, 21, 33);
-  currX = 21;
-  currY = 33;
-  currZoom = 6;
-  showCurrTile();
-  scrollTime(document.getElementById('time').min);
-  document.addEventListener('keyup', handleShortcuts, false);
-}
-
-function getData() {
+function loadDataAtTime(epoch) {
+  document.getElementById('markers').innerHTML = ''; 
+  
+  d1 = new Date(epoch*1000-3600);
+  d2 = new Date(epoch*1000+3600);
+  
   ajax = new XMLHttpRequest();
-   if (!ajax) {
-      alert('Giving up :( Cannot create an XMLHTTP instance');
-      return false;
-    }
   ajax.open('POST', baseurlapi, true);
   ajax.onreadystatechange = showData;
   ajax.setRequestHeader('Content-Type', 'application/json');
@@ -181,14 +165,15 @@ function getData() {
       "during": {
         "ref": "om:phenomenonTime",
         "value": [
-          "2017-01-10T14:00:00+01:00",
-          "2017-01-30T15:00:00+01:00"
+          d1.toISOString(),
+          d2.toISOString()
         ]
       }
     }
   };
   
   ajax.send(JSON.stringify(request));
+  displayCurrentMapParameters();
 }
 
 function showData() {
@@ -217,39 +202,27 @@ function showData() {
   }
 }
 
-function scrollTime(epoch) {
-  document.getElementById('markers').innerHTML = ''; 
-  
-  d = new Date(epoch*1000-3600);
-  d2 = new Date(epoch*1000+3600);
-  
-  ajax = new XMLHttpRequest();
-   if (!ajax) {
-      alert('Giving up :( Cannot create an XMLHTTP instance');
-      return false;
-    }
-  ajax.open('POST', baseurlapi, true);
-  ajax.onreadystatechange = showData;
-  ajax.setRequestHeader('Content-Type', 'application/json');
-  ajax.setRequestHeader('Accept', 'application/json');
-  
-  var request = {
-    "request": "GetObservation",
-    "service": "SOS",
-    "version": "2.0.0",
-    "temporalFilter": {
-      "during": {
-        "ref": "om:phenomenonTime",
-        "value": [
-          d.toISOString(),
-          d2.toISOString()
-          //"2017-01-10T14:00:00+01:00",
-          //"2017-01-30T15:00:00+01:00"
-        ]
-      }
-    }
-  };
-  
-  ajax.send(JSON.stringify(request));
-  log();
+function handleShortcuts(e) {
+  switch(e.key)
+  {
+    case '+': zoomIn(); break;
+    case '-': zoomOut(); break;
+    case 'w': up(); break;
+    case 'a': left(); break;
+    case 's': down(); break;
+    case 'd': right(); break;
+    case 'q': timeBackward(); break;
+    case 'e': timeForward(); break;
+  }  
+}
+
+function init() {
+  currX = 21;
+  currY = 33;
+  currZoom = 6;
+  maxX = Math.pow(2, 6);
+  maxY = Math.pow(2, 6);
+  showCurrTile();
+  loadDataAtTime(document.getElementById('time').value);
+  document.addEventListener('keyup', handleShortcuts, false);
 }
